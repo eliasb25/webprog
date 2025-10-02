265a8cc5f7244b5c424359261107e1fd63051723
dave241005 <david.voegele@sap.com>
2025-10-02 18:44:21 +0200

    angefangen die Logik für das Hinzufügen zu implementieren

--- js/createFolder.js
+++ js/createFolder.js
@@ -1,3 +1,10 @@
+import { Folder } from "/js/folder.js";
+
 window.addEventListener("load", () => {
-    
+    let createButton = document.getElementById("anlegenButton");
+    createButton.addEventListener("click", () => {
+        let input = document.getElementById("verzeichnis-name");
+        let folder = new Folder(input.value);
+        folders.push(folder);
+    });
 });
