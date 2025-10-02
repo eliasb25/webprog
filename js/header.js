8ced7f150c60d0b5ef16445f16153286c28d2e6a
Elias Buerkle <elias.buerkle@sap.com>
2025-10-02 08:42:55 +0200

    footer in separate file

--- js/header.js
+++ js/header.js
@@ -10,3 +10,16 @@ fetch('header.html') // The path to the HTML file
             .catch(error => {
                 console.error('Error fetching the HTML file:', error);
             });
+
+fetch('footer.html') // The path to the HTML file
+            .then(response => response.text()) // Get the response as text
+            .then(htmlContent => {
+                // Find the container element
+                const container = document.getElementById('footer');
+
+                // Insert the fetched HTML content into the container
+                container.innerHTML = htmlContent;
+            })
+            .catch(error => {
+                console.error('Error fetching the HTML file:', error);
+            });            
