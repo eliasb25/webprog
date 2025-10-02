26d85f631fbe1d04f24fa9b04d6e050866e3f615
dave241005 <david.voegele@sap.com>
2025-10-02 13:13:56 +0200

    Sidebar in extra Datei ausgelagert

--- js/displaySidebar.js
+++ js/displaySidebar.js
@@ -1,12 +1,15 @@
-fetch('header.html') // The path to the HTML file
+
+window.addEventListener("load", () => {
+    fetch('sidebar.html') // The path to the HTML file
     .then(response => response.text()) // Get the response as text
     .then(htmlContent => {
         // Find the container element
-        const container = document.getElementById('header');
+        const container = document.getElementById('sidebar');
 
         // Insert the fetched HTML content into the container
         container.innerHTML = htmlContent;
     })
     .catch(error => {
         console.error('Error fetching the HTML file:', error);
-    });
+    })
+});
